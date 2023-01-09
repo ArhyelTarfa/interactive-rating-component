aeba770c912615ac9b1a517c4348db03a01692eb
Arhyel <arhyeltarfa2@gmail.com>
2023-01-09 10:14:35 +0100

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./CSS/**/*.{html,js}"
+    "./**/*.{html,js}",'./index.html','./index.js',
   ],
   theme: {
     extend: {
